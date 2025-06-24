5ec3c8e1cca91a0335e2dfdf71135bcfe75fea62
aditya-dawadikar <mail.aditya.dawadikar@gmail.com>
2025-06-24 14:54:29 -0700

    feat: add api to fetch version history

---
+++ backend/routes/version_history.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from store import session_cache
+from session_management import list_commits
+
+
+router = APIRouter()
+
+@router.get("/version-history")
+def get_version_history(session_id:str):
+    session_data = session_cache.get(session_id, None)
+
+    if session_data is None:
+        return JSONResponse({
+            "success": False,
+            "msg": f"session_id {session_id} not found"
+        })
+
+    version_history = list_commits(session_data)
+
+    return JSONResponse(version_history)
