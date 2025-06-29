be5c7d225d75163551df2e113407c361df87c36e
aditya-dawadikar <mail.aditya.dawadikar@gmail.com>
2025-06-29 07:55:56 -0700

    feat: add file node in the DAG view

--- frontend/src/components/CommitDAG.tsx
+++ frontend/src/components/CommitDAG.tsx
@@ -1,15 +1,79 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import ReactFlow, { ReactFlowProvider, Background, Controls, useNodesState, useEdgesState, Position } from 'reactflow'
+import React, { useEffect } from 'react'
+import ReactFlow, {
+  ReactFlowProvider,
+  Background,
+  Controls,
+  useNodesState,
+  useEdgesState,
+  Position,
+  Handle
+} from 'reactflow'
 import { useSelector, useDispatch } from 'react-redux'
 import type { RootState } from '../store'
 import { setSelectedCommit } from '../slices/commitSlice'
 import type { Node, Edge } from 'reactflow'
 
+
+// === Custom Commit Node ===
+export const CommitNode = ({ data }: { data: any }) => {
+  return (
+    <div
+      style={{
+        textAlign: 'left',
+        whiteSpace: 'normal',
+        overflowWrap: 'break-word',
+        maxWidth: 220,
+        boxSizing: 'border-box',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: '#54ffc9',
+        border: '2px solid #ccc',
+        borderRadius: 8,
+        padding: 4,
+      }}
+    >
+      <div>
+        <b>Commit ID:</b>{' '}
+        <span style={{ background: '#fdff32', padding: '1px 4px', borderRadius: '2px' }}>
+          {data.commit_id}
+        </span>
+      </div>
+
+      <div style={{ fontSize: '0.75rem', marginTop: 2 }}>
+        <b>Created at:</b> {data.timestamp}
+      </div>
+
+      <div style={{ marginTop: 4 }}>
+        <b>Description:</b>
+        <span
+          style={{
+            display: 'block',
+            backgroundColor: '#f9f9f9',
+            boxShadow: 'inset 1px 1px 3px rgba(0,0,0,0.5)',
+            borderRadius: '4px',
+            padding: '6px',
+            lineHeight: 1.4,
+            boxSizing: 'border-box',
+            width: '100%',
+            marginTop: '2px',
+          }}
+        >
+          {data.key_steps}
+        </span>
+      </div>
+
+      <Handle type="source" position={Position.Right} id="main" />
+      <Handle type="source" position={Position.Bottom} id="files" />
+      <Handle type="target" position={Position.Left} id="incoming" />
+    </div>
+  )
+}
+
+
+const nodeTypes = { commitNode: CommitNode }
+
 const CommitDAG = () => {
-
   const dispatch = useDispatch()
-
-  const sessionId = useSelector((state: RootState) => state.chat.session_id)
 
   const commits = useSelector((state: RootState) => state.commit.commits)
   const commitHead = useSelector((state: RootState) => state.commit.head)
@@ -18,94 +82,85 @@ const CommitDAG = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState([])
   const [edges, setEdges, onEdgesChange] = useEdgesState([])
 
-  const nodeBackgroundColor = '#54ffc9'
-  const selectedNodeBackgroundColor = '#ffe878'
+
+  const nodeSpacingX = 500
+  const fileOffsetX = 300
+  const fileSpacingY = 50
 
   useEffect(() => {
-    const nodeSpacing = 200
+    const initialNodes: Node[] = []
+    const initialEdges: Edge[] = []
 
-    const initialNodes: Node[] = commits.map((commit, idx) => ({
-      id: commit.commit_id,
-      data: {
-        label: (
-          <div
-            style={{
-              textAlign: 'left',
-              whiteSpace: 'normal',
-              overflowWrap: 'break-word',
-              width: '100%',
-              boxSizing: 'border-box',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <div>
-              <b>Commit ID:</b>{' '}
-              <span style={{ background: '#fdff32', padding: '1px 4px', borderRadius: '2px' }}>
-                {commit.commit_id}
-              </span>
-            </div>
+    commits.forEach((commit, idx) => {
+      const baseX = idx * nodeSpacingX
+      const baseY = 100
 
-            <div style={{ fontSize: '0.75rem', marginTop: 2 }}>
-              <b>Created at:</b> {commit.timestamp}
-            </div>
+      // Commit node
+      initialNodes.push({
+        id: commit.commit_id,
+        type: 'commitNode',
+        data: { ...commit },
+        position: { x: baseX, y: baseY },
+      })
 
-            <div style={{ marginTop: 4 }}>
-              <b>Description:</b>
-              <span
-                style={{
-                  display: 'block',
-                  backgroundColor: '#f9f9f9',
-                  boxShadow: 'inset 1px 1px 3px rgba(0,0,0,0.5)',
-                  borderRadius: '4px',
-                  padding: '6px',
-                  lineHeight: 1.4,
-                  boxSizing: 'border-box',
-                  width: '100%',
-                  marginTop: '2px',
-                }}
-              >
-                {commit.key_steps}
-              </span>
-            </div>
-          </div>
-        )
-      },
-      position: { x: idx * nodeSpacing, y: 100 },
-      sourcePosition: Position.Right,
-      targetPosition: Position.Left,
-      style: {
-        border: '2px solid',
-        borderColor: commit.commit_id === commitHead?.commit_id ? 'red' : '#ccc',
-        borderRadius: 8,
-        padding: 4,
-        backgroundColor: nodeBackgroundColor,
-        width: 140,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-      },
-    }))
+      // Parent edge
+      if (commit.parent_id) {
+        initialEdges.push({
+          id: `${commit.parent_id}->${commit.commit_id}`,
+          source: commit.parent_id,
+          sourceHandle: 'main',
+          target: commit.commit_id,
+          targetHandle: 'incoming',
+          animated: true,
+          type: 'smoothstep',
+          style: { stroke: '#888' },
+        })
+      }
 
-    const initialEdges: Edge[] = commits
-      .filter((c) => c.parent_id)
-      .map((c) => ({
-        id: `${c.parent_id}->${c.commit_id}`,
-        source: c.parent_id!,
-        target: c.commit_id,
-        animated: true,
-        type: 'beizer',
-        style: { stroke: '#888' },
-      }))
+      // File nodes
+      if (commit.generated_files?.length) {
+        commit.generated_files.forEach((file, i) => {
+          const fileId = `${commit.commit_id}-file-${i}`
+
+          initialNodes.push({
+            id: fileId,
+            type: 'default',
+            data: { label: file.title },
+            position: {
+              x: baseX + fileOffsetX,
+              y: baseY + 100 + i * fileSpacingY,
+            },
+            targetPosition: Position.Left,
+            style: {
+              backgroundColor: '#fef5d3',
+              border: '1px solid #ccc',
+              borderRadius: 4,
+              padding: 6,
+              width: 140,
+              fontSize: '0.75rem',
+              textAlign: 'center',
+            },
+          })
+
+          initialEdges.push({
+            id: `edge-${commit.commit_id}-${fileId}`,
+            source: commit.commit_id,
+            sourceHandle: 'files',
+            target: fileId,
+            type: 'smoothstep',
+          })
+        })
+      }
+    })
 
     setNodes(initialNodes)
     setEdges(initialEdges)
-  }, [commits, commitHead])
+  }, [commits])
 
-  // Highlight selected commit
   useEffect(() => {
     if (!selectedCommit) return
 
+    // TODO: this color highlight is not working
     setNodes((nds) =>
       nds.map((node) => {
         if (node.id === selectedCommit.commit_id) {
@@ -114,17 +169,11 @@ const CommitDAG = () => {
             style: {
               ...node.style,
               borderColor: 'blue',
-              backgroundColor: selectedNodeBackgroundColor,
             },
           }
         }
         return {
-          ...node,
-          style: {
-            ...node.style,
-            borderColor: node.id === commitHead?.commit_id ? 'red' : '#ccc',
-            backgroundColor: nodeBackgroundColor,
-          },
+          ...node
         }
       })
     )
@@ -133,26 +182,25 @@ const CommitDAG = () => {
   return (
     <ReactFlowProvider>
       <div style={{ width: '100%', height: '100%', overflow: 'auto' }}>
-        <ReactFlowProvider>
-          <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            fitView={false}
-            panOnScroll
-            zoomOnScroll
-            onNodeClick={(_, node) => {
-              const selected = commits.find((c) => c.commit_id === node.id)
-              if (selected) {
-                dispatch(setSelectedCommit(selected))
-              }
-            }}
-          >
-            <Background />
-            <Controls />
-          </ReactFlow>
-        </ReactFlowProvider>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          fitView
+          panOnScroll
+          zoomOnScroll
+          nodeTypes={nodeTypes}
+          onNodeClick={(_, node) => {
+            const selected = commits.find((c) => c.commit_id === node.id)
+            if (selected) {
+              dispatch(setSelectedCommit(selected))
+            }
+          }}
+        >
+          <Background />
+          <Controls />
+        </ReactFlow>
       </div>
     </ReactFlowProvider>
   )
