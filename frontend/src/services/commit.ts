3524aa164448b7ab866d783bdf17aeef5dee2f72
aditya-dawadikar <mail.aditya.dawadikar@gmail.com>
2025-06-29 21:38:33 -0700

    fix: req  url for version history

--- frontend/src/services/commit.ts
+++ frontend/src/services/commit.ts
@@ -2,9 +2,9 @@ const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const getVersionHistory = async (session_id: string): Promise<any> => {
 
-    const response = await fetch(`${BASE_URL}/version-history/?session_id=${session_id}`)
+    const response = await fetch(`${BASE_URL}/version-history?session_id=${session_id}`)
 
-    if (!response.ok){
+    if (!response.ok) {
         throw new Error("Failed to fetch Version History")
     }
 
@@ -12,10 +12,10 @@ export const getVersionHistory = async (session_id: string): Promise<any> => {
 
 }
 
-export const getCommitFiles = async(session_id: string, commit_id: string): Promise<any>=>{
+export const getCommitFiles = async (session_id: string, commit_id: string): Promise<any> => {
     const response = await fetch(`${BASE_URL}/list_commit_files?session_id=${session_id}&commit_id=${commit_id}`)
 
-    if (!response.ok){
+    if (!response.ok) {
         throw new Error("Failed to fetch commit files")
     }
 
