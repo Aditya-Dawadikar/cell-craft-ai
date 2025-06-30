848dfb7cde6a5613eca3ed3bc2b435aaaab8b833
aditya-dawadikar <mail.aditya.dawadikar@gmail.com>
2025-06-29 18:27:57 -0700

    feat: local search project by name feaure

--- frontend/src/components/SideNav.tsx
+++ frontend/src/components/SideNav.tsx
@@ -7,7 +7,9 @@ import {
     Box,
     Typography,
     Dialog, DialogTitle, DialogContent,
-    DialogActions, Button, TextField, InputLabel
+    DialogActions, Button, TextField, InputLabel,
+    Autocomplete,
+    Tooltip
 } from '@mui/material'
 
 import { useState } from 'react';
@@ -24,19 +26,23 @@ import type { Session } from '../interfaces/SessionInterface';
 
 const SideNav = () => {
 
-    const navOptions = [{
-        "display_name": "New Project",
-        "icon": CreateNewFolderIcon,
-    }, {
-        "display_name": "Search Project",
-        "icon": SearchIcon,
-    }]
+    const navOptions = {
+        "NEW_PROJECT": {
+            "display_name": "New Project",
+            "icon": CreateNewFolderIcon,
+        },
+        "SEARCH_PROJECT": {
+            "display_name": "Search Project",
+            "icon": SearchIcon,
+        }
+    }
 
     const [dialogOpen, setDialogOpen] = useState(false)
     const [sessionName, setSessionName] = useState("")
     const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
     const sessionsList = useSelector((state: RootState) => state.session.sessions)
+    const activeSession = useSelector((state: RootState) => state.session.activeSession)
 
     const dispatch = useDispatch()
 
@@ -73,7 +79,6 @@ const SideNav = () => {
     }
 
     const handleSelectSession = async (selectedSession: Session) => {
-
         dispatch(setActiveSession({
             session_id: selectedSession.session_id,
             session_name: selectedSession.session_name
@@ -84,19 +89,61 @@ const SideNav = () => {
         <Box sx={{ width: '100%', height: '100vh', bgcolor: '#f5f5f5' }}>
             <Box sx={{ p: 2 }}>
                 <Typography variant="h6" gutterBottom>
-                    Navigation
+                    CellCraft-AI
                 </Typography>
                 <List>
-                    {navOptions.map((option) => (
-                        <ListItem key={option.display_name} disablePadding>
-                            <ListItemButton onClick={() => setDialogOpen(true)}>
-                                <ListItemIcon>
-                                    <option.icon />
-                                </ListItemIcon>
-                                <ListItemText primary={option.display_name} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
+                    <ListItem key={navOptions.SEARCH_PROJECT.display_name} disablePadding>
+                        <Autocomplete
+                            sx={{ width: 400 }}
+                            size="small"
+                            options={sessionsList}
+                            getOptionLabel={(session) =>
+                                `${session.session_name}`
+                            }
+                            renderOption={(props, option) => {
+                                const { key, ...rest } = props
+                                return (
+                                    <Box key={key} component='li' {...rest}>
+                                        <Tooltip
+                                            title={option.session_name}
+                                            componentsProps={{
+                                                tooltip: {
+                                                    sx: {
+                                                        fontSize: '1rem', // or '20px'
+                                                        maxWidth: 400,
+                                                    }
+                                                }
+                                            }}
+                                            placement="left"
+                                            arrow>
+                                            <span>
+                                                {option.session_name}
+                                            </span>
+                                        </Tooltip>
+                                    </Box>
+                                )
+                            }}
+                            value={
+                                sessionsList.find((s) => s.session_id === activeSession?.session_id) ?? null
+                            }
+                            onChange={(event, newValue) => {
+                                if (newValue) handleSelectSession(newValue)
+                            }}
+                            renderInput={(params) => <TextField {...params} label="Search Projects" />}
+                            isOptionEqualToValue={(option, value) =>
+                                option.session_id === value?.session_id
+                            }
+                        />
+                    </ListItem>
+                    <br />
+                    <ListItem key={navOptions.NEW_PROJECT.display_name} disablePadding>
+                        <ListItemButton onClick={() => setDialogOpen(true)}>
+                            <ListItemIcon>
+                                <navOptions.NEW_PROJECT.icon />
+                            </ListItemIcon>
+                            <ListItemText primary={navOptions.NEW_PROJECT.display_name} />
+                        </ListItemButton>
+                    </ListItem>
                 </List>
 
                 <Typography variant="h6" sx={{ mt: 3 }}>
